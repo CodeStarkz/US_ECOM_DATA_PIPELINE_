12d813275af617f683039940dcd52276f66c2069
abhisheksingh <20mecp01@iiitdmj.ac.in>
2026-04-05 17:56:14 +0530

    enhancement in every defined function make it more readable, that will be helpfull while debuging

--- dags/us_ecom_pipeline_dag.py
+++ dags/us_ecom_pipeline_dag.py
@@ -3,14 +3,19 @@ from pendulum import datetime
 import os
 from airflow.providers.smtp.operators.smtp import EmailOperator
 from airflow.utils.email import send_email
+import posiedan
+
 
 
 # 1. Define the failure callback function outside the DAG
 def notify_failure(context):
     subject = f"FAILED: Task {context['task_instance'].task_id} in {context['dag'].dag_id}"
+    to_send=os.getenv("failure_report_email_id")
+    subject_for_email="FAILURE ALERT!!!"
     html_content = f"The pipeline failed at step: {context['task_instance'].task_id}. Check Airflow logs."
-    # Use the email defined in your .env or a hardcoded one
-    send_email(to='your-email@gmail.com', subject=subject, html_content=html_content)
+    # sending email for failure report
+    send_email(to= to_send,
+               subject=subject_for_email, html_content=html_content)
 
 
 @dag(
@@ -20,7 +25,7 @@ def notify_failure(context):
     catchup=False,
     is_paused_upon_creation=True,
     default_args={
-        # Using a list for the email field as Airflow expects
+        # Using a list for the email field as Airflow expects list of recievers
         'email': ['20mecp01@iiitdmj.ac.in','abhirajpoot01011998@gmail.com'],
         'email_on_failure': True,
         'email_on_retry': False,
@@ -30,8 +35,10 @@ def notify_failure(context):
 def us_ecom_pipeline():
     @task
     def check_for_file_from_Retailer_to_organisation(**kwargs):
+        input_file_name="data.csv"
         import pandas as pd
-        path = "/opt/airflow/data/data.csv"
+        input_path_directory = "/opt/airflow/data/"
+        path = input_path_directory + input_file_name
         pd.read_csv(path, encoding="latin1")  # Just checking if it exists/readable
         kwargs['ti'].xcom_push(key="Retailer_file_path_name", value=path)
         return "File found"
@@ -40,8 +47,11 @@ def us_ecom_pipeline():
     def move_file_from_data_to_intermediate_storage(**kwargs):
         import shutil
         ti = kwargs['ti']
-        src = ti.xcom_pull(key="Retailer_file_path_name", task_ids="check_for_file_from_Retailer_to_organisation")
-        dest = "/opt/airflow/Intermediat_Storage/data.csv"
+        src = ti.xcom_pull(key="Retailer_file_path_name",
+                           task_ids="check_for_file_from_Retailer_to_organisation")
+        dest_path = "/opt/airflow/Intermediat_Storage/"
+        dest_file_name="data.csv"
+        dest= dest_path + dest_file_name
         ti.xcom_push(key="destination_path_file_name", value=dest)
         shutil.move(src, dest)
         return "File moved"
@@ -50,14 +60,17 @@ def us_ecom_pipeline():
     def transform_data(**kwargs):
         import pandas as pd
         ti = kwargs['ti']
-        src = ti.xcom_pull(key="destination_path_file_name", task_ids="move_file_from_data_to_intermediate_storage")
+        src = ti.xcom_pull(key="destination_path_file_name",
+                           task_ids="move_file_from_data_to_intermediate_storage")
         df = pd.read_csv(src, encoding="latin1")
 
         # Transformation Logic
         df["InvoiceNo"] = df["InvoiceNo"].astype("int64", errors="ignore")
         df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
 
-        out_path = "/opt/airflow/S3_Blob_Storage/data_transformed.csv"
+        transformed_file_name="data_transformed.csv"
+        transformed_file_save_directory="/opt/airflow/S3_Blob_Storage/"
+        out_path =  transformed_file_save_directory + transformed_file_name
         df.to_csv(out_path, index=False)
         os.remove(src)
         ti.xcom_push(key="transformed_path_file_name", value=out_path)
@@ -68,15 +81,18 @@ def us_ecom_pipeline():
         import pandas as pd
         from sqlalchemy import create_engine
         engine = create_engine("mysql+mysqldb://airflow:airflow@mysql:3306/usdb")
-        path = kwargs['ti'].xcom_pull(key="transformed_path_file_name", task_ids="transform_data")
+        path = kwargs['ti'].xcom_pull(key="transformed_path_file_name",
+                                      task_ids="transform_data")
         df = pd.read_csv(path, encoding="latin1")
-        df.to_sql("us_ecom_data", con=engine, if_exists="replace", index=False)
+        df.to_sql("us_ecom_data", con=engine,
+                  if_exists="replace", index=False)
         return "Loaded"
 
     @task
     def move_file_from_intermediate_to_backup(**kwargs):
         import shutil
-        src = kwargs['ti'].xcom_pull(key="transformed_path_file_name", task_ids="transform_data")
+        src = kwargs['ti'].xcom_pull(key="transformed_path_file_name",
+                                     task_ids="transform_data")
         dest = "/opt/airflow/Back_Up_Cold_Storage/data_transformed.csv"
         shutil.move(src, dest)
         return "Backup complete"
@@ -84,7 +100,7 @@ def us_ecom_pipeline():
     # 2. Define EmailOperator as a STANDALONE task (not a @task function)
     send_success_email = EmailOperator(
         task_id='send_success_notification',
-        to=['20mecp01@iiitdmj.ac.in','abhirajpoot01011998@gmail.com'],
+        to="email",
         subject='ETL Pipeline Success: {{ dag.dag_id }}',
         html_content="""
             <h3>Pipeline Run Successful</h3>
@@ -92,6 +108,13 @@ def us_ecom_pipeline():
             <p><b>Execution Date:</b> {{ ds }}</p>
         """
     )
+    @task
+    def data_quality_report_to_DA_team(**kwargs):
+        import posiedan as ps
+        input_path_for_posiedan= kwargs["ti"].xcom_pull(key="destination_path_file_name",
+                                                        task_ids="move_file_from_data_to_intermediate_storage")
+        output_path_for_posiedan= "/opt/airflow/S3_Blob_Storage/profilig_report_directory"
+
 
     # 3. Setting up the pipeline flow correctly
     step1 = check_for_file_from_Retailer_to_organisation()
